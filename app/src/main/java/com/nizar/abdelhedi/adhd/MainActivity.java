fbc92cb8eefb9f9ee3fb2746401d1c59147c9685
abdel <abdelhedinizarcss@gmail.com>
2016-10-15 00:24:42 +0200

    Login interface

--- app/src/main/java/com/nizar/abdelhedi/adhd/MainActivity.java
+++ app/src/main/java/com/nizar/abdelhedi/adhd/MainActivity.java
@@ -1,14 +1,44 @@
 package com.nizar.abdelhedi.adhd;
 
+import android.app.ActionBar;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+
+import com.dd.processbutton.iml.ActionProcessButton;
+
+import static android.R.attr.button;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActionProcessButton btnSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        btnSignIn = (ActionProcessButton) findViewById(R.id.btnSignIn);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    btnSignIn.setProgress(50);
+                    btnSignIn.setMode(ActionProcessButton.Mode.PROGRESS);
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                btnSignIn.setProgress(100);
+            }
+        });
+
 
     }
 }
